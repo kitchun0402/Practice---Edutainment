bb43f64b9b26165baedb4fc5c679a576558199a3
Kenneth Hau <hkc930402@gmail.com>
2021-07-22 17:39:42 +0800

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by kit chun on 22/7/2021.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
